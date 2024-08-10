f255b6102b3cc518a8d003c13fef4959c1c4b192
Alex Coder <aleks.coder@yandex.ru>
2024-08-10 16:11:02 +0300

    create class component Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,50 +1,28 @@
-import User from "./User/User";
+import React from "react";
 import s from "./Users.module.css";
+import axios from "axios";
+import User from "./User/User";
 
-const Users = (props) => {
+class Users extends React.Component {
 
-    if (props.users.length === 0) {
-        props.setUsers ([
-            {   
-                id: 1,
-                friend: true,
-                ava: "img/stepik.jpg",
-                name: "Stepanidus Stepikovich",
-                age: "8",
-                location: "Moscow, Russia",
-                status: "last seen Aug 04 at 16:16"
-            },
-            {   
-                id: 2,
-                friend: false,
-                ava: "https://cdn-edge.kwork.ru/files/avatar/large_r/44/4289180-2.jpg",
-                name: "Alex Coder",
-                age: "18",
-                location: "Moscow, Russia",
-                status: "online"
-            },
-            {   
-                id: 3,
-                friend: true,
-                ava: "img/ivan.jpg",
-                name: "Ivan Polyakov",
-                age: "19",
-                location: "Tumen, Russia",
-                status: "online"
-            },
-        ])
+    constructor(props) {
+        super(props);
+        axios.get("http://localhost:3001/list").then(response => {
+            this.props.setUsers(response.data);
+            console.log(response.data);
+        })
     }
+    render() {
 
-    let usersElements = props.users.map(e => <User key={e.id} id={e.id} friend={e.friend} ava={e.ava} name={e.name} age={e.age} location={e.location} status={e.status} addFriend={props.addFriend} removeFriend={props.removeFriend}/>);
+        let usersElements = this.props.users.map(e => <User key={e.id} id={e.id} friend={e.friend} ava={e.ava} name={e.name} age={e.age} location={e.location} status={e.status} addFriend={this.props.addFriend} removeFriend={this.props.removeFriend} />);
 
-    return (
-        <div className={s.Users}>
+        return <div className={s.Users}>
             <h3>Users</h3>
             <div className={s.usersBlock}>
                 {usersElements}
             </div>
         </div>
-    )
+    }
 }
 
 export default Users;
