0a40043286c48a0302c210f0fe1af388c1094004
Alex Coder <aleks.coder@yandex.ru>
2024-10-11 17:48:05 +0300

    l75 - create login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,32 @@
+import s from './Login.module.css';
 
+const LoginForm = (props) => {
+    return (
+        <form>
+            <div>
+                <input type="text" placeholder="Input login" name='login' />
+            </div>
+            <div>
+                <input type="text" placeholder="Input password" name='passwd' />
+            </div>
+            <div className={s.checkbox}>
+                <input type="checkbox" name='remember' /> Remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
 
 const Login = (props) => {
-    return <h1>Login</h1>
+    return (
+        <div className={s.Login}>
+            <h1>Login</h1>
+            <LoginForm />
+        </div>
+
+    )
 }
 
 export default Login;
